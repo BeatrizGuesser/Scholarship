f093967b7764fa102431f53b2287ef14da0c156c
Beatriz <biabagg12@gmail.com>
2023-08-15 13:54:46 -0300

    Added SQL script that populates

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO tb_coordinator (name_coordinator)
+VALUES ('Coordinator 1'), ('Coordinator 2'), ('Coordinator 3');
+
+INSERT INTO tb_scrum (name_scrum)
+VALUES ('Scrum Master 1'), ('Scrum Master 2'), ('Scrum Master 3');
+
+INSERT INTO tb_instructor (name_instructor)
+VALUES ('Instructor 1'), ('Instructor 2'), ('Instructor 3');
+
+INSERT INTO tb_student (name_student)
+VALUES ('Student 1'), ('Student 2'), ('Student 3'), ('Student 4'), ('Student 5'), ('Student 6'), ('Student 7'),
+('Student 8'), ('Student 9'), ('Student 10'), ('Student 11'), ('Student 12'), ('Student 13'), ('Student 14');
